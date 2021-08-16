5d44cfcbbe1085e796a70dc5ae188e9c52f329c3
masha23678 <71825234+masha23678@users.noreply.github.com>
2021-08-16 07:51:51 -0400

    Add files via upload

---
+++ DrawerNavigator.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {createDrawerNavigator} from '@react-navigation/drawer'
+import StackNavigator from './StackNavigator'
+import Profile from "../screens/Profile"
+import Logout from '../screens/LogoutScreen'
+const Drawer = createDrawerNavigator()
+const DrawerNavigator = () => {
+    return(
+        <Drawer.Navigator>
+            <Drawer.Screen name="Home" component={StackNavigator} /> 
+
+            <Drawer.Screen name="Profile" component={Profile} />
+            <Drawer.Screen name="Logout" component={Logout} />
+        </Drawer.Navigator>
+    )
+}
+export default DrawerNavigator;
---
+++ config.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBhDaxlGupz39_D3zT0Qak2kmLVeMfpZAM",
+    authDomain: "storytellingapp-aa34a.firebaseapp.com",
+    databaseURL: "https://storytellingapp-aa34a-default-rtdb.firebaseio.com",
+    projectId: "storytellingapp-aa34a",
+    storageBucket: "storytellingapp-aa34a.appspot.com",
+    messagingSenderId: "120439846903",
+    appId: "1:120439846903:web:09c7d939c5533a6b5160f2"
+  };
+  
